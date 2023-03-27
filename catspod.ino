a211b29954b45ffcfce6be943ab858839333960c
Alexandre <alexandre.bernard@badakan.com>
2023-03-27 17:37:22 +0200

    wip

--- catspod.ino
+++ catspod.ino
@@ -1,104 +1,131 @@
-/*
-  The circuit:
-  - Note: on most Arduinos there is already an LED on the board
-    attached to 13
-*/
-/*
+
+#include <Arduino.h>
+#include <SPI.h>
 #include <Wire.h>
-#include <DS3231.h>
+#include <PubSubClient.h>
+#include <WiFiNINA.h>
 
-const int buttonPin = 7;     
-const int relayPin =  8;     
-const int ledPin =  13;  
-const char* potPIN =  "A0";      
-struct Horaire {
-  int hour;
-  int min;
-  int sec;
-  };
-RTClib rtcObject;
-//init distribution time table
-const Horaire distrib[4] = {{9,0,0}, {16,50,0}, {17,19,0}, {22,50,0}};
+#include "secrets.h"
+
+void setup_wifi();
+void reconnect();
+
+static char payload[256];
 
 
+#define TOKEN ""
+#define DEVICEID "Cat_feeder"
 
 
-void setup() {
-  Serial.begin(9600);
-  Wire.begin();
-  //init pin mode
-  pinMode(relayPin, OUTPUT);
-  digitalWrite(relayPin, HIGH);
-  pinMode(ledPin , OUTPUT);  
-  pinMode(2 , OUTPUT);  
-  pinMode(buttonPin, INPUT_PULLUP);    
-  //for init only
-  //initDate();
-}
+const int buttonPin = 2;     
+const int relayPin =  8;
+const int wifiLEDPin =  9;
+const int powerLEDPin =  10;
+const char potPIN =  A0;   
+const char* ssid = SECRET_SSID;
+const char* password = SECRET_PASS;
+const char mqtt_server[] = "192.168.29.250";
+const char subscribeTopic[] = "cmnd/catfeeder/POWER";
+const char publishTopic[] = "stat/catfeeder/POWER";
 
-void loop() {
-  //blink led
-  digitalWrite(LED_BUILTIN, HIGH);   
-  delay(1000);              
-  digitalWrite(LED_BUILTIN, LOW);
-  //retreive date from RTC Module 
-  DateTime dateTime = rtcObject.now();   
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (checkTimeTable(dateTime) || digitalRead(buttonPin) == LOW) {
-    int sensorValue = analogRead(A0);
-    int timeToRun = sensorValue * 10;
-    runMotor(timeToRun);
+WiFiClient wifiClient;
+PubSubClient mqtt(wifiClient);
+
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.print(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
   }
-  serialPrintTime(dateTime);
-  //delay(500);
+  Serial.println(" CONNECTED");
+  digitalWrite(wifiLEDPin, HIGH);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
-bool checkTimeTable(DateTime time){
-    for(int i =0; i < 4; i++){
-      if( distrib[i].hour == time.hour() &&
-          distrib[i].min == time.minute() &&
-          distrib[i].sec == time.second()
-          )
-          return true;
+void setup_mqtt(){
+  mqtt.setServer(mqtt_server, 1883);
+  mqtt.setCallback(callback);//Déclaration de la fonction de souscription
+  reconnect();
+}
+
+void reconnect() {
+  while (!mqtt.connected()) {
+    Serial.print("Attempting MQTT connection .... ");
+    if (mqtt.connect(DEVICEID, MQTTUSER, MQTTPASS)) {
+      Serial.println(" CONNECTED");
+    }else{
+      Serial.print(" FAILED to connect to MQTT broker, rc=");
+      Serial.print(mqtt.state());
+      Serial.println("try again in 5 second");
+      delay(5000);
     }
-    return false;
+  }
+  mqtt.subscribe(subscribeTopic);
 }
 
-void runMotor(int timeToRun){
-  digitalWrite(2, HIGH);
+void runMotor(){
+  int sensorValue = analogRead(potPIN);
+  int timeToRun = 1000;//sensorValue * 10;
+  mqtt.publish(publishTopic, "ON");
+  digitalWrite(powerLEDPin, LOW);
   Serial.print("motorRun - ");
   Serial.print(timeToRun);
   Serial.println("ms");
   digitalWrite(relayPin, LOW);
   delay(timeToRun);
   digitalWrite(relayPin, HIGH);
-  digitalWrite(2, LOW);
+  digitalWrite(powerLEDPin, HIGH);
+  mqtt.publish(publishTopic, "OFF");
 }
 
-void serialPrintTime(DateTime time){
-  Serial.print("date : ");
-  Serial.print(time.day());
-  Serial.print("/");
-  Serial.print(time.month());
-  Serial.print("/");
-  Serial.print(time.year());
-  Serial.print("\t Heure : ");
-  Serial.print(time.hour());
-  Serial.print(":");
-  Serial.print(time.minute());
-  Serial.print(".");
-  Serial.println(time.second());  
+void setup() {
+  //init serial connection
+  Serial.begin(9600);
+  delay(3000); //waiting serial connection 
+  Serial.println("---------------");
+  Serial.println(" Cat Feeder V2 ");
+  Serial.println("---------------");
+  //init pin mode
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(powerLEDPin, OUTPUT);
+  pinMode(wifiLEDPin, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(relayPin, OUTPUT);
+
+  digitalWrite(powerLEDPin, HIGH);
+  digitalWrite(relayPin, HIGH);
+  setup_wifi();
+  setup_mqtt(); 
 }
 
-//use for init DS3231 only 
-void initDate(){
-  DS3231 Clock;
-  Clock.setClockMode(false);  // false set to 24h, true to 12h
-  Clock.setYear(2023);
-  Clock.setMonth(3);
-  Clock.setDate(12);
-  Clock.setDoW(6);
-  Clock.setHour(00);
-  Clock.setMinute(00);
-  Clock.setSecond(01);
-}*/
+void callback(char *topic, byte *payload, unsigned int length) {
+  Serial.print("- mqtt msg - ");
+  Serial.print(topic);
+  Serial.println(" -");
+  //if(topic == "cmnd/catfeeder/POWER" /*&& (char) payload == 'ON'*/){
+    runMotor();
+  //}
+}
+
+void loop() {
+  if (!mqtt.connected()) {
+    reconnect();
+  }
+  mqtt.loop();
+  //mqtt.subscribe()
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (digitalRead(buttonPin) == LOW) {
+    runMotor();
+  }
+  //test here
+  Serial.println(digitalRead(buttonPin));
+  
+  //Serial.print("potar ");
+  //Serial.println(analogRead(potPIN));
+  delay(100);
+}
